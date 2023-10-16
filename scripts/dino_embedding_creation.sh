6bea7db159db5e3ac7616f31375aa90578cb1d49
FranziskaCapeAnalytics <franziska@capeanalytics.com>
2023-10-16 19:56:48 +0200

    Update scripts for dino embedding creation

--- scripts/dino_embedding_creation.sh
+++ scripts/dino_embedding_creation.sh
@@ -1,6 +1,7 @@
 mkdir /cnvrg/output/dino_checkpoint;
 gsutil cp $1 /cnvrg/output/dino_checkpoint/teacher_checkpoint.pth;
 gsutil cp $2 /cnvrg/output/dino_checkpoint/config.yaml;
+pip install fvcore
 pip install xformers==0.0.18
 python /cnvrg/scripts/dino_embedding_creation.py --data /data/dino_fixed_rg_evaluation_imagenet --config /cnvrg/output/dino_checkpoint/config.yaml --checkpoint /cnvrg/output/dino_checkpoint/teacher_checkpoint.pth --outpath /cnvrg/output/rgevaluation;
 cd /cnvrg/output/rgevaluation/embeddings;
@@ -13,4 +14,7 @@ cnvrgv2 dataset put -n $3 -f .
 
 """
 bash /cnvrg/scripts/dino_embedding_creation.sh 'gs://cape-ml-projects-data/data_stores/dinov2/experiments/a100x4/DIN-145/eval/training_112499/teacher_checkpoint.pth' 'gs://cape-ml-projects-data/data_stores/dinov2/experiments/a100x4/DIN-145/config.yaml' rg_embeddings_dino_145_112499
+bash /cnvrg/scripts/dino_embedding_creation.sh 'gs://cape-ml-projects-data/data_stores/dinov2/experiments/a100x4/DIN-121/eval/training_124999/teacher_checkpoint.pth' 'gs://cape-ml-projects-data/data_stores/dinov2/experiments/a100x4/DIN-121/config.yaml' rg_embeddings_dino_121_124999
+
+
 """
