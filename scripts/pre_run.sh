12888e1bfed56bf5b68d527947bcc06d4dd409a0
vikrant <vikrant@capeanalytics.com>
2023-12-19 21:20:48 +0000

    add prerun

---
+++ scripts/pre_run.sh
@@ -0,0 +1,6 @@
+cd /cnvrg;
+pip install fvcore;
+pip install xformers==0.0.18;
+pip install neptune;
+git clone https://github.com/capeanalytics/pj_cape_foundation_eval.git;
+pip install /cnvrg/pj_cape_foundation_eval/pj_cape_foundation_eval;
